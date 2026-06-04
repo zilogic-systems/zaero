6927d2d90de8615d7baa1c071d3a80c7bb1d96bc
manikandan.s <manikandan.s@zilogic.com>
2026-06-04 15:49:09 +0530

    Add modified wheel file and updated test suite

--- test/test_ssid_config.py
+++ test/test_ssid_config.py
@@ -2,12 +2,15 @@ import zaero
 import pytest
 import time
 
-def test_config_ssid(initialize):
-    ssid = initialize.read_from_database("controller", "mld_ssid_3")
-    initialize.set_ssid("controller", "mld_iface_index", ssid)
+def test_config_ssid_1(initialize):
+    ssid = initialize.read_from_database("controller", "mld_ssid_2")
+    initialize.set_ssid("controller", "mld_iface_index", ssid, 'gui')
     for i in range(1, 31):
         try:
-            initialize.check_ssid("controller", "mld_iface_index", ssid)
+            initialize.check_ssid("controller", "mld_iface_index", ssid, 'cli')
+            curr_ssid = initialize.get_ssid("controller", "2g_ssid_index", 'de')
+            if curr_ssid != ssid:
+                print(f"Both SSIDs are not matched {ssid} : {curr_ssid}")
         except Exception as ERR:
             print(f"ERROR: {ERR}")
         else:
@@ -16,3 +19,4 @@ def test_config_ssid(initialize):
     else:
         raise Exception("SSID is not changed in DUT by checking with iw command")
     time.sleep(10)
+
