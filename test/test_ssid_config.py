9aad4ede24ceee71b8493750324e2cb44e60e0f9
manikandan.s <manikandan.s@zilogic.com>
2026-06-25 13:25:02 +0530

    Cleanup Test suites

--- test/test_ssid_config.py
+++ test/test_ssid_config.py
@@ -3,71 +3,6 @@ import pytest
 import time
 import os
 
-def test_config_ssid_with_extenders(initialize):
-    ssid = initialize.read_from_database("controller", "mld_ssid_3")
-    initialize.set_ssid("controller", "mld_iface_index", ssid, 'gui')
-    for i in range(1, 31):
-        try:            
-            initialize.check_ssid("controller", "mld_iface_index", ssid, 'cli')
-            initialize.check_ssid("extender1", "mld_iface_index", ssid, 'cli')
-            initialize.check_ssid("extender2", "mld_iface_index", ssid, 'cli')
-        except Exception as ERR:
-            initialize.zi_logger.log(f"ERROR: {ERR}")
-        else:
-            break
-        time.sleep(5)
-    else:
-        raise Exception("SSID is not changed in DUT by checking with iw command")
-    time.sleep(10)
-
-def test_config_ssid_with_packet_capture_and_download(initialize):
-    ssid = initialize.read_from_database("controller", "mld_ssid_2")
-    pcap_file_name = "sample_6"
-    pcap_remote_dir = initialize.read_from_database("controller", "pcap_remote_dir")
-    pcap_local_dir = initialize.read_from_database("controller", "pcap_local_dir")
-    backhaul_iface = initialize.read_from_database("controller", "backhaul_capture_iface")
-    frame_filter = initialize.read_from_database("controller", "filter_1905")
-
-    initialize.set_sniffer_log_file("controller", pcap_file_name)
-    initialize.start_frame_capture("controller", backhaul_iface, frame_filter)
-    initialize.set_ssid("controller", "mld_iface_index", ssid, 'gui')
-
-    for i in range(1, 31):
-        try:
-            initialize.check_ssid("controller", "mld_iface_index", ssid, 'cli')
-        except Exception as ERR:
-            print(f"ERROR: {ERR}")
-        else:
-            break
-        time.sleep(5)
-    else:
-        raise Exception("SSID is not changed in DUT by checking with iw command")
-
-    pcap_remote_dir = os.path.join(pcap_remote_dir, pcap_file_name + '.pcapng')
-    initialize.download_pcap("controller", pcap_remote_dir, pcap_local_dir)
-    initialize.delete_pcap("controller", f"{pcap_file_name}.pcapng")
-    time.sleep(10)
-
-def test_config_ssid_with_packet_capture(initialize):
-    ssid = initialize.read_from_database("controller", "mld_ssid_3")
-    pcap_file_name = "sample_1"
-    backhaul_iface = initialize.read_from_database("controller", "backhaul_capture_iface")
-    frame_filter = initialize.read_from_database("controller", "filter_1905")
-    initialize.set_sniffer_log_file("controller", pcap_file_name)
-    initialize.start_frame_capture("controller", backhaul_iface, frame_filter)
-    initialize.set_ssid("controller", "mld_iface_index", ssid, 'gui')
-    for i in range(1, 31):
-        try:            
-            initialize.check_ssid("controller", "mld_iface_index", ssid, 'cli')
-        except Exception as ERR:
-            print(f"ERROR: {ERR}")
-        else:
-            break
-        time.sleep(5)
-    else:
-        raise Exception("SSID is not changed in DUT by checking with iw command")
-    time.sleep(10)
-
 def test_config_ssid(initialize):
     ssid = initialize.read_from_database("controller", "mld_ssid_2")
     initialize.set_ssid("controller", "mld_iface_index", ssid, 'gui')
@@ -86,7 +21,19 @@ def test_config_ssid(initialize):
         raise Exception("SSID is not changed in DUT by checking with iw command")
     time.sleep(10)
 
-
-
-
-
+def test_config_ssid_with_extenders(initialize):
+    ssid = initialize.read_from_database("controller", "mld_ssid_3")
+    initialize.set_ssid("controller", "mld_iface_index", ssid, 'gui')
+    for i in range(1, 31):
+        try:            
+            initialize.check_ssid("controller", "mld_iface_index", ssid, 'cli')
+            initialize.check_ssid("extender1", "mld_iface_index", ssid, 'cli')
+            initialize.check_ssid("extender2", "mld_iface_index", ssid, 'cli')
+        except Exception as ERR:
+            initialize.zi_logger.log(f"ERROR: {ERR}")
+        else:
+            break
+        time.sleep(5)
+    else:
+        raise Exception("SSID is not changed in DUT by checking with iw command")
+    time.sleep(10)
--- test/test_tunneling_and_reboot.py
+++ test/test_tunneling_and_reboot.py
@@ -3,62 +3,6 @@ import pytest
 import time
 import os
 
-
-# def test_config_ssid_with_extenders(initialize):
-#     ssid = initialize.read_from_database("controller", "mld_ssid_3")
-#     initialize.set_ssid("controller", "mld_iface_index", ssid, 'gui')
-#     for i in range(1, 31):
-#         try:            
-#             initialize.check_ssid("controller", "mld_iface_index", ssid, 'cli')
-#             initialize.check_ssid("extender1", "mld_iface_index", ssid, 'cli')
-#         except Exception as err:
-#             print(err)
-#         else:
-#             break
-#         time.sleep(5)
-#     else:
-#         raise Exception("SSID is not changed in DUT by checking with iw command")
-#     time.sleep(10)
-#     initialize.reboot_device("extender1", "cli")
-#     initialize.reboot_device("controller", "cli")
-#     initialize.close_connection("controller")
-#     initialize.close_connection("extender1")
-#     time.sleep(10)
-
-#     for i in range(1, 41):
-#         print (f"FOR loop iteration : {i}")
-#         try:
-#             initialize.connect_with_device("controller")
-#         except:
-#             print("Connection FAILED with 'Controller'")
-#         else:
-#             print("Connection SUCCESS with 'Controller'")
-#             break
-#         time.sleep(10)
-
-#     for i in range(1, 41):
-#         print (f"FOR loop iteration : {i}")
-#         try:
-#             initialize.connect_with_device("extender1")
-#         except:
-#             print("Connection FAILED with 'Extender1'")
-#         else:
-#             print("Connection SUCCESS with 'Extender1'")
-#             break
-#         time.sleep(10)
-
-#     for i in range(1, 31):
-#         try:            
-#             initialize.check_ssid("controller", "mld_iface_index", ssid, 'cli')
-#             initialize.check_ssid("extender1", "mld_iface_index", ssid, 'cli')
-#         except Exception as err:
-#             print(err)
-#         else:
-#             break
-#         time.sleep(5)
-#     else:
-#         raise Exception("SSID is not changed in DUT by checking with iw command")
-
 def test_wifi_reset(initialize: zaero.zaero):
     initialize.wifi_reset("controller", 'gui')
     initialize.reboot_device("extender1", "cli")
@@ -103,9 +47,3 @@ def test_wifi_reset(initialize: zaero.zaero):
     else:
         raise Exception("SSID is not changed in DUT by checking with iw command")
     time.sleep(10)
-   
-   
-    
-
-    
-
